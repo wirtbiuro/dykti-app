e329a4620933aba6237ed8da90d91a1fa499a7bb
IvanNess <chai@bk.ru>
2022-09-19 23:00:14 +0200

    test 2

--- hooks/useCalendarData.tsx
+++ hooks/useCalendarData.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 export const useCalendarData = () => {
     const [check, __setCheck] = useState()
-    const [isChecked, __setIsChecked] = useState()
+    const [isChecked, __setIsChecked] = useState<boolean>(false)
     const [value, __setValue] = useState()
     const [setValue, __setSetValue] = useState()
     const [showError, __setShowError] = useState()
