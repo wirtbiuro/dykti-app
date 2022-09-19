4d27aea2634eaeba6b45040d0c7c2ffa6ea7b650
IvanNess <chai@bk.ru>
2022-09-19 23:04:49 +0200

    error fixing 4

--- hooks/useCalendarData.tsx
+++ hooks/useCalendarData.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
+import { DateTime } from 'luxon'
 
 export const useCalendarData = () => {
     const [check, __setCheck] = useState()
     const [isChecked, __setIsChecked] = useState<boolean>(false)
-    const [value, __setValue] = useState()
+    const [value, __setValue] = useState<DateTime | null | string>()
     const [setValue, __setSetValue] = useState()
     const [showError, __setShowError] = useState()
     const [errTitleElement, __setErrTitleElement] = useState()
